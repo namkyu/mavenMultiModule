d24f2049c6fd733251d4116280486241792c42da
namkyu <lng1982@gmail.com>
2013-03-28 10:15:44 +0900

    maven multi module test

---
+++ comm/src/main/java/com/kyu/component/service/UserService.java
@@ -0,0 +1,15 @@
+package com.kyu.component.service;
+
+/**
+ * @FileName : UserService.java
+ * @Project : comm
+ * @Date : 2013. 3. 27.
+ * @작성자 : 이남규
+ * @프로그램설명 :
+ */
+public class UserService {
+
+	public String getUser() {
+		return "user1234";
+	}
+}
---
+++ user/src/main/java/com/kyu/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.kyu.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kyu.component.service.UserService;
+
+/**
+ * Servlet implementation class UserController
+ */
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("user!!!!!!");
+		String userId = new UserService().getUser();
+		System.out.println(userId);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
